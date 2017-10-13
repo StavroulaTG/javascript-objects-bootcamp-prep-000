818c79b407f66f2ccc1342295a0743340731513b
Stavroula Guerrero <4stacyg@optonline.net>
2017-10-13 21:03:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var playlist = {'artistnames' : 'songtitles'}
 function updatePlaylist(playlist, artistnames, songtitles) {
   return Object.assign({}, playlist, {['Phil Ochs'] :"Here's to the State of Mississippi"})
@@ -6,3 +7,6 @@ function removeFromPlaylist(playlist, artistname) {
 delete playlist[artistname]
   return playlist
 }
+=======
+var playlist = {artistnames : songtitles}
+>>>>>>> 59bb88cca029da2ca66218a794e81b87c490f235
